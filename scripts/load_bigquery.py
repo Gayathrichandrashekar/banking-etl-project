0c6e6c044d2bf71ad56556595fbb933a09a10bab
Gayathrichandrashekar <2325devichandra@gmail.com>
2026-05-31 16:47:07 +0530

    Fixed GitHub MySQL connection

--- scripts/load_bigquery.py
+++ scripts/load_bigquery.py
@@ -1,9 +1,24 @@
+import os
+import json
 import pandas as pd
-from google.cloud import bigquery
 
-# Service Account Key
-client = bigquery.Client.from_service_account_json(
-    "credentials/airflow-key.json"
+from google.cloud import bigquery
+from google.oauth2 import service_account
+
+# Read credentials from GitHub Secret
+credentials_info = json.loads(
+    os.environ["GCP_CREDENTIALS"]
+)
+
+# Create credentials object
+credentials = service_account.Credentials.from_service_account_info(
+    credentials_info
+)
+
+# Create BigQuery client
+client = bigquery.Client(
+    credentials=credentials,
+    project=credentials_info["banking-etl-project"]
 )
 
 # Read transformed file
@@ -11,8 +26,10 @@ df = pd.read_csv(
     "data/customer_summary.csv"
 )
 
+# BigQuery Table
 table_id = "banking-etl-project.banking_data.customer_summary"
 
+# Load Data
 job = client.load_table_from_dataframe(
     df,
     table_id
