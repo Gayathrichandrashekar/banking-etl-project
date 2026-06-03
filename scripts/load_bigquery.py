c45ec2dfe37b510dd267eebb082ca8517dab1320
Gayathrichandrashekar <2325devichandra@gmail.com>
2026-06-03 12:12:59 +0530

    fix3

--- scripts/load_bigquery.py
+++ scripts/load_bigquery.py
@@ -1,35 +1,26 @@
-import os
-import json
-import pandas as pd
-
 from google.cloud import bigquery
 from google.oauth2 import service_account
+import pandas as pd
+import os
+import json
 
-# Read credentials from GitHub Secret
 credentials_info = json.loads(
     os.environ["GCP_CREDENTIALS"]
 )
 
-# Create credentials object
 credentials = service_account.Credentials.from_service_account_info(
     credentials_info
 )
 
-# Create BigQuery client
 client = bigquery.Client(
     credentials=credentials,
     project=credentials_info["banking-etl-project"]
 )
 
-# Read transformed file
-df = pd.read_csv(
-    "data/customer_summary.csv"
-)
+df = pd.read_csv("customer_summary.csv")
 
-# BigQuery Table
 table_id = "banking-etl-project.banking_data.customer_summary"
 
-# Load Data
 job = client.load_table_from_dataframe(
     df,
     table_id
